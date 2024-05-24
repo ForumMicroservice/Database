598559b8d100ff221f0607bebe172fc044cfe061
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2024-05-24 21:11:57 +0300

    Override abstract  method for Roles migration

--- src/migrations/1716161082809-Roles.ts
+++ src/migrations/1716161082809-Roles.ts
@@ -3,6 +3,7 @@ import { MigrationOperations } from "./abstractMigration/abstractMigration";
 import { v4 as uuidv4 } from 'uuid';
 import { roleTable } from "./migrationEntity/role";
 
+
 export class Roles1716161082809 extends MigrationOperations implements MigrationInterface{
     constructor(queryRunner:QueryRunner){
         super(queryRunner);
@@ -13,11 +14,11 @@ export class Roles1716161082809 extends MigrationOperations implements Migration
         try{
             if(await this.getDatabase(queryRunner,"forum"))
             {
-               await this.getTable(queryRunner,"Roles") == false ? await this.createTable(queryRunner,"Roles",roleTable) : await this.createRecords(queryRunner,"Roles",['id','name'], ['superuser', 'admin', 'user']); 
+               await this.getTable(queryRunner,"Roles") == false ? await this.createTable(queryRunner,"Roles",roleTable) : await this.createRecords(queryRunner); 
             }else{
                console.error("Migration::Method up:::Database isn't exist") 
             }    
-        }catch(error){
+        }catch(error){ 
             console.error("Migration::Method up:::Database error::",`${error}`);
         }
    }  
@@ -31,6 +32,15 @@ export class Roles1716161082809 extends MigrationOperations implements Migration
             } 
         }catch(err){
             console.error("Migration:: Method Down :: Error database because of: \n",err);
+        }
+    }
+
+    public override async createRecords(queryRunner:QueryRunner) : Promise<any>{
+
+        if(queryRunner){
+            const isTable = await this.getTable(queryRunner,"Roles") == true ? await queryRunner.query(`INSERT INTO Roles (id, name) VALUES ('${uuidv4()}', 'superadmin'),
+                                                                                                                                            ('${uuidv4()}', 'admin'), ('${uuidv4()}', 'users')`) : false
+            return isTable;               
         }
     }
 }
